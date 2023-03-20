24d75a69da3a073c05e569aa15db24445692b185
Alekssandr Shitov <shitovaleksand@mail.ru>
2023-03-20 22:41:29 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { ShopItemClass } from './components/ShopItemClass';
 import './App.css';
 
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
 function App() {
-  const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-    price: 399,
-    currency: '£'
-  }
   return (
     <div className="container">
       <div className="background-element">
--- src/components/ShopItemClass.jsx
+++ src/components/ShopItemClass.jsx
@@ -2,27 +2,19 @@ import React, { Component } from 'react'
 
 export class ShopItemClass extends Component {
 render () {
-  const item = {
-  brand: 'Tiger of Sweden',
-  title: 'Leonard coat',
-  description: 'Minimalistic coat in cotton-blend',
-  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-  price: 399,
-  currency: '£'
-}
-
+  const { brand, title, description, descriptionFull, currency, price } = this.props.item;
   return (
     <div className="main-content">
-        <h2>{item.brand}</h2>
-        <h1>{item.title}</h1>
-        <h3>{item.description}</h3>
-        <div className="descriptionFull">{item.descriptionFull}</div>
+        <h2>{brand}</h2>
+        <h1>{title}</h1>
+        <h3>{description}</h3>
+        <div className="descriptionFull">{descriptionFull}</div>
         <div className="highlight-window-mobile">
           <div className="highlight-overlay"></div>
         </div>
         <div className="divider"></div>
         <div className="purchase-info">
-          <div className="price">{`${item.currency}${item.price}.00`}</div>
+          <div className="price">{`${currency}${price}.00`}</div>
           <button>Добавить в корзину</button>
         </div>
       </div>
